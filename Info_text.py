e42eb0764c57659a97595b419f1897c044784449
kimberly-moors <kimberly.moors@gmail.com>
2017-01-04 22:26:55 +0100

    Addition of commentary

--- Info_text.py
+++ Info_text.py
@@ -1,6 +1,10 @@
-filename = "I am going crazy. Because I just keep programming." # temporary
+"""
+This file prints several bits of information about the text string.
+"""
 from  word_level import * 			#star imports all the functions in the library
 from function_lib import *
+
+filename = "The second reason for his delay was a personal one. He had dawdled over his cigar because he was at heart a dilettante, and thinking over a pleasure to come often gave him a subtler satisfaction than its realization. This was especially the case when the  pleasure was a delicate one, as his pleasures mostly were; and on this occasion the moment he looked forward to was so rare and exquisite in quality that - well, if he had timed his arrival in accord with the prima donna's stage manager he could not have entered the Academy at a more significant moment than just as she was singing and sprinkling the falling daisy petals with notes as clear as dew." # temporary
 words = list(load_input(filename))
 dictionary= load_dictionary("data\\dictionaryABC.csv", ';')
 lemmas = lematization(words)
