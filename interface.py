94a65c660946eaddd6845a9fafe17d49250f51c5
kimberly-moors <kimberly.moors@gmail.com>
2017-01-04 21:09:46 +0100

    Construction of the analyse definition

--- interface.py
+++ interface.py
@@ -42,10 +42,10 @@ class Application(Frame):
 		self.button_frame = Frame(self)
 		self.button_frame.pack(side='top', expand = 0, fill = X)
 
-		self.submit_button = Button(self.button_frame, text = "Submit", command = self.reveal)
+		self.submit_button = Button(self.button_frame, text = "Submit", command = self.analyse)
 		self.submit_button.pack(padx=2, pady=2, side=LEFT)
 
-		self.simplify_button = Button(self.button_frame, text= "Make easier", command= self.reveal)
+		self.simplify_button = Button(self.button_frame, text= "Make easier", command= self.analyse)
 		self.simplify_button.pack(padx=2, pady=2, side=LEFT)
 
 		
@@ -70,14 +70,17 @@ class Application(Frame):
 #	def retrieve_input():
 #		input = self.test.get("1.0",'end-1C')
 
-	def reveal(self): # Tekst verbonden aan knop 'test ' --> wat getoond word
+	def analyse(self): # Tekst verbonden aan knop 'test ' --> wat getoond word
+		global lexicon, level
 		self.info.configure(state ='normal') # open witing in box
-		content = self.input.get(1.0,"end-1c")
-
-		if content == "Hallo":
-			message = "YES"
-		else:
-			message = "Shit"
+		filename= self.input.get(1.0,"end-1c")
+		words = list(load_input(filename))
+		dictionary= load_dictionary("data\\dictionaryABC.csv", ';')
+		lemmas = lematization(words)
+		lexicon = lexicon(dictionary,words,lemmas)
+		level = str(level(lexicon,dictionary))
+		sentences= load_input2(filename)
+		message = "The level of the text: " + level + "\n" + "The number of words: "+ str(nr_words(words)) + "\n" + r' ' + "\n" + 'The average length of the words: ' + av_length_words(words) + "\n" + 'The longest word: ' + str(max_length_words(words)) + "\n" + 'The length of the longest word: ' + len_longest_word(words) + "\n" + 'The differentation of words within the text is: ' + differentiation(words, filename) + "\n" + "The number of sentences: "+ str(sentence_count(sentences)) + "\n" + "The average length of the sentences: " + av_sentence_length(sentences) + "\n" + "The longest sentence: " + max_sentence(sentences) + "\n" + "The shortest sentence: " + min_sentence(sentences)
 		self.info.delete(0.0, END) # programma leeg maken voor er weer toegevoegd word
 		self.info.insert(0.0, message) #enter your answer
 		self.info.configure(state ='disabled') # close writing in box
