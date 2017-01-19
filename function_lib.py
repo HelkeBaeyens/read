a44c6e230227fdad6e92e6881204aacd089928e6
kimberly-moors <kimberly.moors@gmail.com>
2017-01-19 14:41:17 +0100

    Correction and addition to word recognizer
    Solving the problem of skipped words and recognizing exceptions

--- function_lib.py
+++ function_lib.py
@@ -162,15 +162,12 @@ def simply_sen(filename):
 def search_unknown(filename, dictionary):
 	""" Searches the words that are misspelled/not in the dictionary"""
 	numbers = re.findall('[\d]+', filename)
-	words = filename.lower().replace('.' or '?', ' ').replace('!' or '-',' ').split(' ')
+	words = filename.lower().replace('?', ' ').replace('!',' ').replace('.',' ').replace('-',' ').replace(':',' ').replace(';',' ').replace(',',' ').replace('(',' ').replace(')',' ').replace('[',' ').replace(']',' ').replace('"',' ').replace("'",' ').split(' ')
 	unknowns = list(words)
 	rex= ['teeth','geese','men','feet','children', 'i','its', 'are','oxen','mice','men','women','sheep', 'me', 'mine', 'his', 'hers', 'there', 'theirs', 'yours', 'her','him', 'their', 'your', 'them', 'my', 'our', 'ours', 'them','people', 'am','these', 'those','could','might','would','should', 'rose','woke','bore','beat', 'been','became','began','bent','bound','bid','bound','bet','bit','bled','blew','broke','bred','brought','built','burned','bought','cast','caught','chose','clung','clothed','came','cost','crept','cut','dealt','dug','proved','dove','drew','dreamt','drank','drove','dwelt','ate','fell','fed','felt','fought','found','fitted','fled','flung','flew','forbade','forgot','forgave','forsook','froze','got','gave','went','ground','grew','hung','had','heard','hewed','hid','hit','held','hurt','inputted','kept','knelt','knit','knew','laid','led','leant','leapt','learnt','does','doing' ,'goes', 'left','lent','let','lay','lied','lit','lost','made','meant','met','mistook','misunderstood','mowed','paid','pled','prepaid','proofread','put','quitted','read','relayed','rid','rode','rang','rose','ran','sawed','said','saw','sought','sold','send','set','sewed','shook','shaved','sheared','shed','shone','shat','shot','showed','shrank','shut','sang','sank','sat','slew','slept','slid','slung','slunk','slit','smelt','snuck','sowed','spoke','sped','spelt','spent','spilt','spun','spit','spilt','spoilt','spread','sprang','stood','stole','stuck','stung','stank','strewed','strode','struck','strung','strove','swore','sweat','swept','swelled','swam','swung','took','taught','tore','told','thought','threw','thrust','trod','woke','wore','wove','wed','wept','wet','whetted','won','wound','withdrew','withheld','withstood','wrung','wrote', 'risen','woken','born','beaten','become','begun','bent','bound','bitten','bound','bet','bitten','bled','blown','broken','bred','brought','built','burnt','bought','cast','caught','chosen','clung','clad','come','cost','crept','cut','dealt','dug','proven','dived','drawn','dreamed','drunk','driven','dwelled','eaten','fallen','fed','felt','fought','found','fit','fled','flung','flown','forbidden','forgotten','forgiven','forsaken','frozen','gotten','given','gone','ground','grown','hung','had', 'has','heard','hewn','hidden','hit','held','hurt','inputted','kept','kneeled','knitted','known','laid','led', 'leaned','leaped','learned','left','lent','let','lain','lied','lighted','lost','made','meant','met','mistaken','misunderstood','mown','paid','pleaded','prepaid','proofread','put','quitted','read','relayed','rid','ridden','rung','risen','run','sawn','said','seen','sought','sold','sent','set','sewn','shaken','shaven','shorn','shed','shined','shitted','shot','shown','shrunk','shut','sung','sunk','sat','slayed','slept','slid','slung','slinked','slit','smelled','sneaked','sowed', 'spoken','speeded','spelled','spent','spilled','spun','spat','split','spoiled','spread','sprung','stood','stolen','stuck','stung','stunk','strewed','stridden','stricken','strung','strived','sworn','sweated','swept','swollen','swum','swung','taken','taught','torn','told','thought','thrown','thrust','trodden','woken','worn','weaved','wedded','wept','wetted','whetted','won','wound','withdrawn','withheld','withstood','wrung','written','were', 'was', "didn't", "wasn't", "couldn't", "wouldn't", "shouldn't", "i'm", "he's", "she's", "you're", "we're", "doesn't", "aren't", "they're", "i'll", "he'll", "we'll", "you'll", "she'll", "it'll", "they'll", "it's", "i'd", "you'd", "he'd", "she'd", "we'd", "they'd"]
 	for word in words:
-		if word == '' or word == r'\W':
+		if word == '':
 			unknowns.remove(word)
-		elif word[-1:] == '\?' or word[-1:] == ',' or word[-1:] == ':' or word[-1:] == ';' or word[-1:] == ')':
-			unknowns.remove(word)
-			unknowns.append(word[:-1])
 	for word in words:
 		if word in dictionary:
 			unknowns.remove(word)
@@ -179,61 +176,70 @@ def search_unknown(filename, dictionary):
 		if word in numbers:
 			unknowns.remove(word)
 
-	for word in unknowns:
+	for word in words:
 		if word[-5:] == 'tting' or word[-5:] =='nning' or word[-5:] == 'pping' or word[-5:] == 'mming' or word[-5:] == 'rring' or word[-5:] == 'lling' or word[-5:] == 'bbing' or word[-5:] == 'ation':
-			if word[:-5] in dictionary:
+			if word[:-4] in dictionary and word in unknowns:
 				unknowns.remove(word)
-			if word[:-4]+'e' in dictionary:
+			if word[:-4]+'e' in dictionary and word in unknowns:
 				unknowns.remove(word)
-		if word[-1:] == 'a':
-			if word[:-1]+'um' in dictionary:
+			if word[:-5] in dictionary and word in unknowns:
 				unknowns.remove(word)
 		if word[-4:] == 'tted' or word[-4:] == 'rred' or word[-4:] == 'lled' or word[-4:] == 'bbed' or word[-4:] == 'gged' or word[-4:] == 'pped':
-			if word[:-3] in dictionary:
+			if word[:-3] in dictionary and word in unknowns:
 				unknowns.remove(word) 
-			if word[:-3] +'e' in dictionary:
+			if word[:-3] +'e' in dictionary and word in unknowns:
 				unknowns.remove(word)
-		if word[-4:] == 'ment' or word[-4:] == 'ness' or word[-4:] == 'ship' or word[-4:] == 'less' or word[-4:] == 'able' or word[-4:] == 'ance': 
-			if word[:-4] in dictionary:
+		if word[-4:] == 'ment' or word[-4:] == 'ness' or word[-4:] == 'ship' or word[-4:] == 'less' or word[-4:] == 'able' or word[-4:] == 'ance' or word[-4:] == 'tion' or word[-4:] == 'sion' or word[-4:]=='ling' or word[-4:] == 'hood' or word[-4:] == 'ical' or word[-4:]=='some': 
+			if word[:-4] in dictionary and word in unknowns:
 				unknowns.remove(word)
-
-	for word in unknowns:
-		if word[-3:] == 'ing':
-			if word[:-3]+'e' in dictionary:
+			if word [:-4]+'e' in dictionary and word in unknowns:
+				unknowns.remove(word)
+			if word [:-3]+'e' in dictionary and word in unknowns:
+				unknowns.remove(word)
+		if word[-3:] == 'ing' or word[-3:] == 'dom' or word[-3:] == 'ize' or word[-3:] == 'ise' or word[-3:] == 'ism'or word[-3:] == 'ful' or word[-3:] == 'ish'or word[-3:] == 'ary'or word[-3:] == 'ate'or word[-3:] == 'ade' or word[-3:] == 'ity' or word[-3:] =='ies' or word[-3:]=='ves'or word[-3:] == 'ive' or word[-3:] == 'age' :
+			if word[:-3]+'e' in dictionary and word in unknowns:
+				unknowns.remove(word)
+			if word[:-3] in dictionary and word in unknowns:
+				unknowns.remove(word)
+			if word[:-3]+'y' in dictionary and word in unknowns:
+				unknowns.remove(word)
+			if word[:-3]+'f' in dictionary and word in unknowns:
+				unknowns.remove(word)
+			if word[:-4]+'ze' in dictionary and word in unknowns:
+				unknowns.remove(word)
+		if word[-2:] == 'es' or word[-2:] == 'ly' or word[-2:] == 'en' or word[:2] =='ic' or word[-2:] == 'or' or word[-2:] == 'al' or word[-2:] == 'ed' or word[-2:] == 'er' or word[-2:] == 'ty' or word[-2:]=='ac':
+			if word[:-2] in dictionary and word in unknowns:
+				unknowns.remove(word)
+			if word[:-2]+'e' in dictionary and word in unknowns:
+				unknowns.remove(word)	
+			if word[:-2]+'a' in dictionary and word in unknowns:
 				unknowns.remove(word)
 		if word [-1:] == 's' or word[-1:] == 'y' or word[-1:] == 'd' or word[-1:] == 'r':
-			if word[:-1] in dictionary:
+			if word[:-1] in dictionary and word in unknowns:
 				unknowns.remove(word)
-
-	for word in unknowns:
-		if word[-3:] == 'ing' or word[-3:] == 'dom' or word[-3:] == 'ize' or word[-3:] == 'ise' or word[-3:] == 'ism'or word[-3:] == 'ful' or word[-3:] == 'ish'or word[-3:] == 'ary'or word[-3:] == 'ate'or word[-3:] == 'ade' or word[-3:] == 'ity':
-			if word[:-3] in dictionary:
-				unknowns.remove(word)
-		if word[-2:] == 'es' or word[-2:] == 'ly' or word[-2:] == 'en' or word[-2:] == 'or' or word[-2:] == 'en' or word[-2:] == 'al' or word[-2:] == 'ed' or word[-2:] == 'er' or word[-2:] == 'ty' :
-			if word[:-2] in dictionary:
-				unknowns.remove(word)
-
-	for word in unknowns: 
 		if word [:7] == 'counter':
-			if word[7:] in dictionary:
+			if word[7:] in dictionary and word in unknowns:
 				unknowns.remove(word)
 		if word [:6] == 'contra':
-			if word[6:] in dictionary:
+			if word[6:] in dictionary and word in unknowns:
 				unknowns.remove(word)
 		if word[:5] == 'extra' or word[:5] == 'hyper' or word[:5] == 'intra' or word[:5] == 'trans' or word[:5] == 'ultra' or word[:5] == 'onder' or word[:5] == 'super' :
-			if word[5:] in dictionary:
-				unknowns.remove(word)
+			if word[5:] in dictionary and word in unknowns:
+				unknowns.remove(word) 
 		if word[:4] == 'ante' or word[:4] == 'anti' or word[:4] == 'hemi' or word[:4] == 'hypo' or word[:4] == 'peri' or word[:4] == 'semi' or word[:4] == 'over' or word[:4] == 'post' or word[:4] == 'auto' or word[:4] == 'mega':
-			if word[4:] in dictionary:
+			if word[4:] in dictionary and word in unknowns:
 				unknowns.remove(word)
 		if word[:3] == 'dis' or word[:3] == 'non' or word[:3] == 'pre' or word[:3] == 'pro' or word[:3] == 'sub' or word[:3] == 'sup' or word[:3] == 'mis':
-			if word[3:] in dictionary:
+			if word[3:] in dictionary and word in unknowns:
 				unknowns.remove(word)
 		if word[:2] == 'un' or word[:2] == 'in' or word[:2] == 'im' or word[:2] == 're' or word[:2] == 'an' or word[:2] == 'af' or word[:2] == 'al' or word[:2] == 'be' or word[:2] == 'co' or word[:2] == 'ex' or word[:2] == 'en' or word[:2] == 'up':
-			if word[2:] in dictionary:
+			if word[2:] in dictionary and word in unknowns:
 				unknowns.remove(word)
 		if word[:1] == 'a' or word[:1] == 'e':
-			if word[1:] in dictionary:
+			if word[1:] in dictionary and word in unknowns:
+				unknowns.remove(word)
+		if word[-1:] == 'a':
+			if word[:-1]+'um' in dictionary and word in unknowns:
 				unknowns.remove(word)
 
 	for word in unknowns:
